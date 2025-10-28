16557e58bd8a1d163db8e6cad25596e029d37fc0
lijo_ferdinand <lijo_ferdinand@thbs.com>
2025-10-28 12:23:36 +0530

    db

--- churn_prediction/settings.py
+++ churn_prediction/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'churn_prediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'churn_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',   # or 'postgres-db' if Django runs in Docker
+        'PORT': '5432',
     }
 }
 
