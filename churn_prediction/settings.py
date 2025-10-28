28374e68eba41c924c0f41afddb08ee4328866d9
lijo_ferdinand <lijo_ferdinand@thbs.com>
2025-10-28 15:27:31 +0530

    adda

--- churn_prediction/settings.py
+++ churn_prediction/settings.py
@@ -96,7 +96,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "officiallijo54@gmail.com"
-EMAIL_HOST_PASSWORD = "ThbsThbs123!"
+EMAIL_HOST_PASSWORD = "jrxt uiyl uixq jivi"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
