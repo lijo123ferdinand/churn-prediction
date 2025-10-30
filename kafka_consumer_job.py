673f80aed98bb97dfcff42e106c7221b1e370f3f
lijo_ferdinand <lijo_ferdinand@thbs.com>
2025-10-30 15:57:14 +0530

    sdf

--- kafka_consumer_job.py
+++ kafka_consumer_job.py
@@ -1,37 +1,28 @@
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors.kafka import (
-    KafkaSource,
-    KafkaOffsetsInitializer
-)
-from pyflink.common.serialization import SimpleStringSchema
+from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer
 from pyflink.common.typeinfo import Types
+from pyflink.common.watermark_strategy import WatermarkStrategy
 
 def main():
     env = StreamExecutionEnvironment.get_execution_environment()
 
-    # Create Kafka source
     source = (
         KafkaSource.builder()
         .set_bootstrap_servers("localhost:9092")
-        .set_topics("user_events")
-        .set_group_id("flink-user-events-consumer")
+        .set_topics("test-topic")
+        .set_group_id("flink_consumer")
         .set_starting_offsets(KafkaOffsetsInitializer.earliest())
         .set_value_only_deserializer(SimpleStringSchema())
         .build()
     )
 
-    # Add source to Flink environment
     ds = env.from_source(
         source,
-        watermark_strategy=None,
+        watermark_strategy=WatermarkStrategy.for_monotonous_timestamps(),
         source_name="kafka_source",
         type_info=Types.STRING()
     )
 
-    # Print messages to stdout
     ds.print()
 
-    env.execute("Kafka User Events Consumer")
-
-if __name__ == "__main__":
-    main()
+    env.execute("Kafka Consumer Job")
